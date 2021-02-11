bf0711f1341bb1416174e34956b6d5debd89f450
ShotaroMatsuya <matsuya@h01.itscom.net>
2021-02-11 21:12:02 +0900

    createing server

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/key');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback', //after user grants permission, user will be sent to this URL from google
+    },
+    (accessToken, refreshToken, profile, done) => {
+      //Get user details & create new record in database here
+      console.log('access token', accessToken); //accessTokenは自動的に期限切れになる
+      console.log('refresh token', refreshToken); //refreshTokenはaccessTokenをupdateできる
+      console.log('profile:', profile);
+    }
+  )
+);
+//scope specifies to google server what access we want to have inside of this user profile
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
